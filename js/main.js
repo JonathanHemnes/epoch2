132adb5664d427055d902c0bc4947f7c10efdbc3
Jonathan Hemnes <jonathan.hemnes@talentkode.com>
2017-04-02 17:30:31 -0600

    splash

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 (function () {
-    
+    document.addEventListener("DOMContentLoaded", function () {
+        Typed.new(".main-type", {
+            strings: ["You're in the right place. ^1000 You know what we do."],
+            typeSpeed: 0,
+            startDelay: 1000,
+            loop: false,
+            showCursor: false
+        });
+    });
 })();
