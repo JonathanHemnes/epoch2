66d78e517d590e735dfcb74d21a0c1a5a5409a83
Jonathan Hemnes <jonathan.hemnes@talentkode.com>
2017-04-02 16:03:56 -0600

    more layouts

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 (function() {
     console.log('hello jonathan')
     document.addEventListener("DOMContentLoaded", function(){
-		Typed.new(".element", {
+		Typed.new(".services-list", {
 			strings: ["First sentence.", "Second sentence."],
 			typeSpeed: 0,
             loop: true
