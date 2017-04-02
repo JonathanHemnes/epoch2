da91aa6834f985fad485580241ce7242e0917881
Jonathan Hemnes <jonathan.hemnes@talentkode.com>
2017-04-02 17:27:12 -0600

    services

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-(function() {
-    console.log('hello jonathan')
-    document.addEventListener("DOMContentLoaded", function(){
-		Typed.new(".services-list", {
-			strings: ["First sentence.", "Second sentence."],
-			typeSpeed: 0,
-            loop: true
-		});
-	});
+(function () {
+    
 })();
